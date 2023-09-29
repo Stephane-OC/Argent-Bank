766bc63a76cd69e4f95b992a9e83e9376751b315
Stephane-OC <stephane.fons.pro@gmail.com>
2023-09-29 09:11:36 +0200

    First Commit project Setup

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignInPage from './pages/SignInPage';
+import UserPage from './pages/UserPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
